67160322420eb56091bfb742d25b9241bace1803
faddy <fadfoud500@gmail.com>
2023-08-13 18:11:36 +0100

    cleaned this shit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import NavBar from "./NavBar";
 import Carousel from "./Carousel";
 function App() {
   return (
-    <>
+    <div className="App">
       <Carousel></Carousel>
       <div className="NavBar"><NavBar/></div>
-    </>
+    </div>
   );
 }
 

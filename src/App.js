dc12e168435a53c0365e8725ea049f3d51919de6
faddy <fadfoud500@gmail.com>
2023-08-13 15:34:12 +0100

    lol2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import { useState } from 'react';
-import './App.css';
+
+import NavBar from './NavBar';
+import './NavBar.css';
 
 function App() {
-  const [state, setState] = useState({count: 0, theme:'blue'})
-  const count = state.count
-  const theme = state.theme
-  function minus() {
-    if (count > 0) {
-      setState(prevState => {
-        return{...prevState,count: prevState.count - 1}
-      })
-    }
-  }
-  function plus() {
-    setState(prevState => {
-      return { ...prevState, count: prevState.count + 1 }
-    })
-  }
-  return (
-    <>
-      <button onClick={minus} className='btn'>-</button>
-      <span className='num'>{count}{theme}</span>
-      <button onClick={plus} className='btn'>+</button>
-    </>
-  );
+  <NavBar/>
 }
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+const NavBar = () => {
+    const [state, setState] = useState({count: 0, theme:'blue'})
+    const count = state.count
+    const theme = state.theme
+    function minus() {
+      if (count > 0) {
+        setState(prevState => {
+          return{...prevState,count: prevState.count - 1}
+        })
+      }
+    }
+    function plus() {
+      setState(prevState => {
+        return { ...prevState, count: prevState.count + 1 }
+      })
+    }
+    return (
+      <>
+        <button onClick={minus} className='btn'>-</button>
+        <span className='num'>{count}{theme}</span>
+        <button onClick={plus} className='btn'>+</button>
+      </>
+    );
+}
+export default NavBar

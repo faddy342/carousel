e1e8ac83a82f0105e4c69ded11bdb12bf605dbc1
faddy <fadfoud500@gmail.com>
2023-08-13 15:05:37 +0100

    lol

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  
-  const [count, setcount] = useState(0)
+  const [state, setState] = useState({count: 0, theme:'blue'})
+  const count = state.count
+  const theme = state.theme
   function minus() {
     if (count > 0) {
-      setcount(count - 1)
+      setState(prevState => {
+        return{...prevState,count: prevState.count - 1}
+      })
     }
   }
   function plus() {
-    setcount(count + 1)
+    setState(prevState => {
+      return { ...prevState, count: prevState.count + 1 }
+    })
   }
   return (
     <>
       <button onClick={minus} className='btn'>-</button>
-      <span className='num'>{count}</span>
+      <span className='num'>{count}{theme}</span>
       <button onClick={plus} className='btn'>+</button>
     </>
   );
